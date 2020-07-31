9728dc398f96f917c80e1bb2092443bfa8c8e62f
Jacob Armstrong <jacob@rootlessdesigns.com>
2020-07-31 12:11:44 -0400

    404 page

--- 404.php
+++ 404.php
@@ -11,48 +11,13 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
-
+        <div class="container">
 		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'charmer' ); ?></h1>
-			</header><!-- .page-header -->
-
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'charmer' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'charmer' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$charmer_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'charmer' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$charmer_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
+                        <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/404-illustration_1.png" alt="You've reached a 404 page.">
+                        <h1 class=""><?php esc_html_e( 'Something&rsquo;s Missing', 'charmer' ); ?></h1>
+                        <p><?php esc_html_e( 'Unfortunately, we don&rsquo;t have anything to show you here. We hope you can find what you were looking for on another page.' , 'charmer' ); ?></p>
+		</section><!-- .error-404 -->    
+        </div><!-- .container -->
 
 	</main><!-- #main -->
 
